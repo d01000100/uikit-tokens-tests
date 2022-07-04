5258daa2f0826a871201d064b4500df1663f1a85
gris <griselda.juarez@frogdesign.com>
2022-07-03 19:14:16 -0500

    Initial Commit

---
+++ MyProyectStoryboardUITests/MyProyectStoryboardUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MyProyectStoryboardUITestsLaunchTests.swift
+//  MyProyectStoryboardUITests
+//
+//  Created by Griselda Juarez Morales on 03/07/22.
+//
+
+import XCTest
+
+class MyProyectStoryboardUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
