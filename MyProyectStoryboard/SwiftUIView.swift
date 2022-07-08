d91c5af8b5d3d2c2537f1db01fe709da48d9c57a
gris <griselda.juarez@frogdesign.com>
2022-07-08 11:48:28 -0500

    Proyecto usando UIKit consumiendo un componente en SwiftUI

---
+++ MyProyectStoryboard/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  MyProyectStoryboard
+//
+//  Created by Griselda Juarez Morales on 07/07/22.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
--- MyProyectStoryboard/ViewController.swift
+++ MyProyectStoryboard/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import SwiftUI
+import ReusableText
+import ReusableText
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        let controller = UIHostingController(rootView: ReusableText())
+        controller.view.translatesAutoresizingMaskIntoConstraints = false
+        self.addChild(controller)
+        self.view.addSubview(controller.view)
+        controller.didMove(toParent: self)
+        
+        NSLayoutConstraint.activate([
+            controller.view.widthAnchor.constraint(equalToConstant: 200),
+            controller.view.heightAnchor.constraint(equalToConstant: 44),
+            controller.view.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            controller.view.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+            ])
     }
 
 
